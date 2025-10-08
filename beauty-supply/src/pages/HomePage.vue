d1413cc4f3b75411fa9e940a0bb1312288ac7c9f
masterworkDEV <godwinkelvin92@yahoo.com>
2025-10-08 18:34:52 +0100

    new new

--- beauty-supply/src/pages/HomePage.vue
+++ beauty-supply/src/pages/HomePage.vue
@@ -13,7 +13,6 @@ import Hero from '@/components/home-page/Hero.vue'
 import NewArrival from '@/components/home-page/NewArrival.vue'
 import WhatOurCustomersSay from '@/components/home-page/WhatOurCustomersSay.vue'
 import BestSellers from '@/components/home-page/BestSellers.vue'
-import Contact from '@/components/home-page/Contact.vue'
 </script>
 
 <style>
